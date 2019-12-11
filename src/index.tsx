23d872346bf4d2665d3b43449adf13da7333e3ac
Zhu Hanming <hanming@huawen.com>
2019-12-11 23:50:50 +0800

    feat: wrap app in index.js and add whydidyourender

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app';
-import * as serviceWorker from './serviceWorker';
+import whyDidYouRender from '@welldone-software/why-did-you-render';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import App from 'app';
+// import AppProviders from 'contexts';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import * as serviceWorker from './serviceWorker';
+import './index.css';
+
+import store, { persistor } from 'app/store';
+
+const render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        {/* <AppProviders> */}
+        <App />
+        {/* </AppProviders> */}
+      </PersistGate>
+    </Provider>,
+    document.getElementById('root')
+  );
+};
+
+render();
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./app/App', render);
+  whyDidYouRender(React);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
