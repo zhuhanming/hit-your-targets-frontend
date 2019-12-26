6a9c4ecbe75261002158e8d7bb67d773b3118373
Hanming Zhu <hanming@huawen.com>
2019-12-26 08:43:41 +0800

    style: comment out unnecessary imports and format code for App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from '../logo.svg';
 
-import { retryPromise } from 'utils';
+// import { retryPromise } from 'utils';
 import { useUser } from 'contexts/userContext';
 import Loading from 'components/loading';
 import './App.scss';
 
 const loadAuthenticatedApp = () => import('./AuthenticatedApp');
 const AuthenticatedApp = React.lazy(loadAuthenticatedApp);
-const UnauthenticatedApp = React.lazy(()=> import('./UnauthenticatedApp'));
+const UnauthenticatedApp = React.lazy(() => import('./UnauthenticatedApp'));
 
 const App: React.FC = () => {
   const user = useUser();
@@ -16,16 +15,12 @@ const App: React.FC = () => {
   React.useEffect(() => {
     loadAuthenticatedApp();
   }, []);
-  
+
   return (
     <React.Suspense fallback={<Loading />}>
-      {user ? (
-        <AuthenticatedApp />
-      ) : (
-        <UnauthenticatedApp />
-      )}
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
     </React.Suspense>
   );
-}
+};
 
 export default App;
