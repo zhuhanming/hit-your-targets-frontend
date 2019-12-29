0126ee37ce6d51f78e93cb19d9355fe90796d7bc
Hanming Zhu <hanming@huawen.com>
2019-12-29 14:09:14 +0800

    feat: add progress overview item ghost

---
+++ src/routes/main/ProgressOverview/ProgressOverviewItem/ProgressOverviewItemGhost.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+
+import './ProgressOverviewItemGhost.scss';
+
+const ProgressOverviewItem = () => {
+  return (
+    <li className="progress-item">
+      <SkeletonTheme color="#C3C3C3">
+        <div className="progress-item__content progress-item__content-ghost">
+          <span className="progress-item__title progress-item__title-ghost">
+            <Skeleton />
+          </span>
+          <span className="progress-item__date progress-item__date-ghost">
+            <Skeleton />
+          </span>
+        </div>
+        <div className="progress-item__content progress-item__content-ghost">
+          <span className="progress-item__bar progress-item__bar-ghost">
+            <Skeleton />
+          </span>
+        </div>
+      </SkeletonTheme>
+    </li>
+  );
+};
+
+export default ProgressOverviewItem;
