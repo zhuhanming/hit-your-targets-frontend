7067c383c3964e200e67876e17eb158c7cb7d4c0
Hanming Zhu <hanming@huawen.com>
2019-12-29 13:43:31 +0800

    feat: build progress overview

---
+++ src/routes/main/ProgressOverview/ProgressOverviewItem/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import { getDisplayDate, isWarning, getDaysRemaining } from 'utils/timeUtils';
+import { getProgress } from 'utils/progressUtils';
+import { useTheme } from 'contexts/themeContext';
+
+import './ProgressOverviewItem.scss';
+
+const ProgressOverviewItem = ({ todo, currentKey, keyLimit }) => {
+  const { theme } = useTheme();
+  const { title, endTime, completed, subtodos } = todo;
+  const displayDate = getDisplayDate(endTime);
+  const warning = isWarning(endTime);
+  const daysRemaining = getDaysRemaining(endTime, completed);
+  const progressAmount = completed ? 100 : getProgress(subtodos);
+
+  return (
+    <li
+      className={`progress-item ${
+        currentKey < keyLimit ? 'has-bottom-border' : ''
+      }`}
+    >
+      <div className="progress-item__content">
+        <span className="progress-item__title">{title}</span>
+        <span
+          className={`progress-item__date ${
+            // eslint-disable-next-line no-nested-ternary
+            completed ? '' : warning ? 'has-text-danger' : ''
+          }`}
+        >
+          {displayDate}
+        </span>
+      </div>
+      <progress
+        className={`progress progress-item__bar ${theme} `}
+        value={`${progressAmount}`}
+        max="100"
+      >
+        {progressAmount}
+      </progress>
+      <div className="progress-item__content">
+        <span
+          className={`progress-item__amount ${
+            completed ? 'has-text-success' : ''
+          }`}
+        >
+          {progressAmount}% COMPLETED
+        </span>
+        <span
+          className={`progress-item__days ${
+            // eslint-disable-next-line no-nested-ternary
+            completed ? '' : warning ? 'has-text-danger' : ''
+          }`}
+        >
+          {daysRemaining}
+        </span>
+      </div>
+    </li>
+  );
+};
+
+export default ProgressOverviewItem;
---
+++ src/routes/main/ProgressOverview/index.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable no-param-reassign */
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { LIST } from 'constants/routes';
+import ProgressOverviewItem from './ProgressOverviewItem';
+
+import './ProgressOverview.scss';
+
+const ProgressOverview = ({ todos, isLoading }) => {
+  if (isLoading) return <></>;
+  if (todos.length > 3) {
+    const completedTodos = todos.filter(todo => !todo.completed);
+    if (completedTodos.length >= 3) {
+      todos = completedTodos.slice(0, 3);
+    } else {
+      todos = todos
+        .slice()
+        .sort((a, b) =>
+          // eslint-disable-next-line no-nested-ternary
+          a.completed === b.completed ? 0 : a.completed ? 1 : -1
+        )
+        .slice(0, 3);
+    }
+  }
+  const { length } = todos;
+  return (
+    <div className="tasks">
+      <ul>
+        {todos.map((ele, key) => {
+          return (
+            <ProgressOverviewItem
+              todo={ele}
+              currentKey={key}
+              key={`progress-item-${ele.id}`}
+              keyLimit={length - 1}
+            />
+          );
+        })}
+        {todos.length === 0 && (
+          <article className="message is-info">
+            <span className="message-body tasks__empty-text">
+              You have not created any tasks yet!
+            </span>
+          </article>
+        )}
+      </ul>
+      <button type="button" className="is-size-7 tasks__cta as-non-button">
+        <Link to={LIST}>View More</Link>
+      </button>
+    </div>
+  );
+};
+
+export default ProgressOverview;
