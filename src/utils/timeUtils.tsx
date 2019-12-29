42e1e6eb6def88aa1f76b9904d31e717a4caf3d3
Hanming Zhu <hanming@huawen.com>
2019-12-29 11:04:55 +0800

    feat: build more support functions for time
    Added the isTomorrow and isThisWeek functions

--- src/utils/timeUtils.tsx
+++ src/utils/timeUtils.tsx
@@ -7,10 +7,41 @@ const isToday = (date: Date) => {
   );
 };
 
+const isTomorrow = (date: Date) => {
+  const today = new Date();
+  const tomorrow = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 1
+  );
+  return (
+    date.getDate() === tomorrow.getDate() &&
+    date.getMonth() === tomorrow.getMonth() &&
+    date.getFullYear() === tomorrow.getFullYear()
+  );
+};
+
+const isThisWeek = (date: Date) => {
+  const today = new Date();
+  const nextWeek = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 6
+  );
+  return date < nextWeek;
+};
+
 const getDisplayDate = (endTime: string) => {
   const endTimeDate = new Date(Date.parse(endTime));
   if (isToday(endTimeDate)) {
     return { displayDate: 'Today', warning: true };
+  }
+  if (isTomorrow(endTimeDate)) {
+    return { displayDate: 'Tomorrow', warning: false };
+  }
+  if (isThisWeek(endTimeDate)) {
+    const week = endTimeDate.toLocaleString('default', { weekday: 'long' });
+    return { displayDate: `${week}`, warning: false };
   }
   const now = new Date();
   const day = endTimeDate.getDate();
