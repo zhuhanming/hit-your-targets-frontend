b57e14a596f4d3f09a8294182a96ae69fdc5a413
Hanming Zhu <hanming@huawen.com>
2019-12-29 14:11:36 +0800

    refactor: clean up unnecessary code and add eslint ignore comments

--- src/components/todoListItem/TodoListItemGhost.tsx
+++ src/components/todoListItem/TodoListItemGhost.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/label-has-for */
 import React from 'react';
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 
@@ -11,20 +13,12 @@ const TodoListItem = () => {
         className="is-checkradio is-success list-item__content__checkbox"
         disabled
       />
-      {/* eslint-disable-next-line jsx-a11y/label-has-for */}
       <label className="list-item__content">
         <div className="list-item__title">
           <SkeletonTheme color="#C3C3C3">
             <Skeleton />
           </SkeletonTheme>
         </div>
-        {/* <div
-          className={`list-item__date is-size-7 ${
-            warning ? 'has-text-danger' : ''
-          }`}
-        >
-          {displayDate}
-        </div> */}
       </label>
     </li>
   );
