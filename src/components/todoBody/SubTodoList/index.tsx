2a127182f9d1d46098c0067f9a0864d61eb4f8b0
Hanming Zhu <hanming@huawen.com>
2020-01-01 08:31:06 +0800

    feat: build subtodolist

--- src/components/todoBody/SubTodoList/index.tsx
+++ src/components/todoBody/SubTodoList/index.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
 
+import SubTodoListItem from './SubTodoListItem';
+import SubTodoCreationField from './SubTodoCreationField';
+
 import './SubTodoList.scss';
 
-const SubTodoList = ({ subtodos }) => {
-  return <></>;
+const SubTodoList = ({ todo }) => {
+  const { id, startTime, endTime, subtodos } = todo;
+  const { length } = subtodos;
+  return (
+    <>
+      <ul>
+        {subtodos.map((subtodo, key) => {
+          return (
+            <SubTodoListItem
+              subTodo={subtodo}
+              currentKey={key}
+              keyLimit={length - 1}
+              todoId={id}
+              todoStartTime={startTime}
+              todoEndTime={endTime}
+              key={`subtodo-${subtodo.id}`}
+            />
+          );
+        })}
+      </ul>
+      <SubTodoCreationField
+        id={id}
+        todoStartTime={startTime}
+        todoEndTime={endTime}
+      />
+    </>
+  );
 };
 
 export default SubTodoList;
