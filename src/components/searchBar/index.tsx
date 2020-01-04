bed77e1f718049170199a4b681565d55ea2f8c69
Hanming Zhu <hanming@huawen.com>
2020-01-04 23:14:07 +0800

    feat: build tags searching

--- src/components/searchBar/index.tsx
+++ src/components/searchBar/index.tsx
@@ -1,29 +1,33 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import TagsInput from 'react-tagsinput';
+import { toast } from 'react-toastify';
 
 import { useView } from 'contexts/viewContext';
 import { useSearch } from 'contexts/searchContext';
 import { View } from 'interfaces/ViewContext';
-import RootStateInterface from 'interfaces/RootState';
+import { capitalize } from 'utils';
 import {
   SearchType,
   setTitleString,
   setTags,
-  setSearchLogic
+  setSearchLogic,
+  setSearch
 } from 'reducers/SearchDux';
 
 import './SearchBar.scss';
 
 const SearchBar = () => {
   const { viewSelected } = useView();
-  const { searchType, titleString } = useSearch();
+  const { searchType, titleString, tags } = useSearch();
   const dispatch = useDispatch();
+  const [isDropdownActive, setIsDropdownActive] = useState(false);
 
   const placeholderText =
     viewSelected === View.COMPLETED
-      ? 'Search through your completed tasks.'
-      : 'Search through your current tasks.';
+      ? 'Search through your completed tasks by its title.'
+      : 'Search through your current tasks by its title.';
 
   const handleChange = e => {
     if (searchType === SearchType.TITLE) {
@@ -31,20 +35,107 @@ const SearchBar = () => {
     }
   };
 
+  const handleChangeSearchType = (newSearchType: SearchType) => {
+    if (newSearchType !== searchType) dispatch(setSearch(newSearchType));
+    setIsDropdownActive(false);
+  };
+
+  const handleTagsInput = (newTags: string[]) => {
+    const newSearchTags = [...new Set(newTags.map(capitalize))];
+    if (newSearchTags.length < newTags.length) {
+      toast.warn('That tag is already being searched for!');
+    } else {
+      dispatch(setTags(newSearchTags));
+    }
+  };
+
+  console.log(tags);
+
   return (
     <form className="search-field">
+      <div
+        className={`dropdown search-field__dropdown ${
+          isDropdownActive ? 'is-active' : ''
+        }`}
+      >
+        <div className="dropdown-trigger">
+          <button
+            type="button"
+            className="button search-field__dropdown__button"
+            aria-haspopup="true"
+            aria-controls="dropdown-menu"
+            onClick={() => setIsDropdownActive(!isDropdownActive)}
+          >
+            <span>
+              {searchType === SearchType.TITLE ? 'By Title' : 'By Tags'}
+            </span>
+            <span className="icon is-small">
+              <i className="fas fa-angle-down" aria-hidden="true" />
+            </span>
+          </button>
+        </div>
+        <div
+          className="dropdown-menu search-field__dropdown__menu"
+          id="dropdown-menu"
+          role="menu"
+        >
+          <div className="dropdown-content search-field__dropdown__content">
+            <button
+              type="button"
+              className={`dropdown-item search-field__dropdown__item ${
+                searchType === SearchType.TITLE ? 'is-active' : ''
+              }`}
+              onClick={() => handleChangeSearchType(SearchType.TITLE)}
+            >
+              By Title
+            </button>
+            <button
+              type="button"
+              className={`dropdown-item search-field__dropdown__item ${
+                searchType === SearchType.TAG ? 'is-active' : ''
+              }`}
+              onClick={() => handleChangeSearchType(SearchType.TAG)}
+            >
+              By Tags
+            </button>
+          </div>
+        </div>
+      </div>
       <div className="control">
-        <input
-          className="input is-info search-field__input"
-          name="title"
-          type="text"
-          autoComplete="off"
-          placeholder={placeholderText}
-          // eslint-disable-next-line jsx-a11y/no-autofocus
-          autoFocus
-          onChange={handleChange}
-          defaultValue={titleString}
-        />
+        {searchType === SearchType.TITLE && (
+          <input
+            className="input is-info search-field__input"
+            name="title"
+            type="text"
+            autoComplete="off"
+            placeholder={placeholderText}
+            // eslint-disable-next-line jsx-a11y/no-autofocus
+            autoFocus
+            onChange={handleChange}
+            defaultValue={titleString}
+          />
+        )}
+        {searchType === SearchType.TAG && (
+          <TagsInput
+            value={tags}
+            onChange={handleTagsInput}
+            tagProps={{
+              className: 'tag is-primary react-tagsinput-tag',
+              classNameRemove: 'react-tagsinput-remove'
+            }}
+            inputProps={{
+              className: 'react-tagsinput-input input tags-field',
+              placeholder: `${
+                // eslint-disable-next-line no-nested-ternary
+                tags.length > 0
+                  ? 'Add tag'
+                  : viewSelected === View.COMPLETED
+                  ? 'Search through your completed tasks by its tags.'
+                  : 'Search through your current tasks by its tags.'
+              }`
+            }}
+          />
+        )}
       </div>
     </form>
   );
