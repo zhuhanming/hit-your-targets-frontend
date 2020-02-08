58f3f338d122647c14a7971f7a8db469078cc760
Hanming Zhu <hanming@huawen.com>
2020-02-08 22:43:33 +0800

    feat: add calendar wip page

---
+++ src/routes/calendar/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import PageContainer from 'components/pageContainer';
+import PageSection from 'components/pageSection';
+
+import './Calendar.scss';
+
+const Calendar = () => {
+  return (
+    <PageContainer titleText="Calendar View">
+      <PageSection>
+        <div className="message is-info">
+          <div className="message-body">
+            This is a work in progress, check back soon!
+          </div>
+        </div>
+      </PageSection>
+    </PageContainer>
+  );
+};
+
+export default Calendar;
